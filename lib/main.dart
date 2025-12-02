be842ef6815febdc5f4ff510c1db0b3b8285dfa6
anana243 <anastasijan2005@gmail.com>
2025-12-02 13:59:27 +0000

    Refactor routing setup to clarify initial route and remove unnecessary comments

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,9 @@ class UnionShopApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF4d2963)),
       ),
-      home: const HomeScreen(),
-      // By default, the app starts at the '/' route, which is the HomeScreen
       initialRoute: '/',
-      // When navigating to '/product', build and return the ProductPage
-      // In your browser, try this link: http://localhost:49856/#/product
       routes: {
-        '/': (context) => const HomeScreen(), // Changed from HomePage()
+        '/': (context) => const HomeScreen(),
         '/shop': (context) => ShopPage(),
         '/print-shack': (context) => PrintShackPage(),
         '/sale': (context) => SalePage(),
