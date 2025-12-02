63bb40737d05f1bb0aa9aa4d959bc492308340de
anana243 <anastasijan2005@gmail.com>
2025-12-02 20:40:44 +0000

    Add AboutPage widget with basic layout structure

---
+++ lib/pages/about_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../app_layout.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+  @override
+  Widget build(BuildContext context) => const AppLayout(title: 'Union', child: Center(child: Text('About Page')));
+}
