008959b421fd4fe8e5fd8881927058c7b158a0ac
anana243 <anastasijan2005@gmail.com>
2025-12-02 20:39:56 +0000

    Add ShopPage widget with basic layout structure

---
+++ lib/pages/shop_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../app_layout.dart';
+
+class ShopPage extends StatelessWidget {
+  const ShopPage({super.key});
+  @override
+  Widget build(BuildContext context) => const AppLayout(title: 'Union', child: Center(child: Text('Shop Page')));
+}
