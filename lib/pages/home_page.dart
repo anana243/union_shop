1578b613fc81d8c0c5a2d60f5cd9b589698d1b44
anana243 <anastasijan2005@gmail.com>
2025-12-02 20:34:27 +0000

    Rename PrintShackPage to SalePage and update the build method to reflect the new page title

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import '../app_layout.dart';
 
-class PrintShackPage extends StatelessWidget {
-  const PrintShackPage({super.key});
+class SalePage extends StatelessWidget {
+  const SalePage({super.key});
   @override
-  Widget build(BuildContext context) => const AppLayout(title: 'Union', child: Center(child: Text('Print Shack Page')));
+  Widget build(BuildContext context) => const AppLayout(title: 'Union', child: Center(child: Text('Sale Page')));
 }
