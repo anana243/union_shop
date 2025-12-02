3e63537cff5175ff2a6323c096597e6a0d4c6709
anana243 <anastasijan2005@gmail.com>
2025-12-02 20:40:10 +0000

    Add PrintShackPage widget with basic layout structure

---
+++ lib/pages/print_shack_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../app_layout.dart';
+
+class PrintShackPage extends StatelessWidget {
+  const PrintShackPage({super.key});
+  @override
+  Widget build(BuildContext context) => const AppLayout(title: 'Union', child: Center(child: Text('Print Shack Page')));
+}
