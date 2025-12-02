0ab9f4dae0420d95d2cd84c8976af8d759b41785
anana243 <anastasijan2005@gmail.com>
2025-12-02 20:41:14 +0000

    Add TermsAndConditionsPage widget with basic layout structure

---
+++ lib/pages/terms_and_conditions_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../app_layout.dart';
+
+class TermsAndConditionsPage extends StatelessWidget {
+  const TermsAndConditionsPage({super.key});
+  @override
+  Widget build(BuildContext context) => const AppLayout(title: 'Union', child: Center(child: Text('Terms and Conditions Content')));
+}
