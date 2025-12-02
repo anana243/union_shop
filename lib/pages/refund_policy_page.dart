66cc1bb8b6c7f0fbfa564c9cb3072d86ba58eb46
anana243 <anastasijan2005@gmail.com>
2025-12-02 20:41:28 +0000

    Add RefundPolicyPage widget with basic layout structure

---
+++ lib/pages/refund_policy_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../app_layout.dart';
+
+class RefundPolicyPage extends StatelessWidget {
+  const RefundPolicyPage({super.key});
+  @override
+  Widget build(BuildContext context) => const AppLayout(title: 'Union', child: Center(child: Text('Refund Policy Content')));
+}
