9c9be3b640f52708ce91ccf1eac2ce622ac48d0e
anana243 <anastasijan2005@gmail.com>
2025-12-02 20:35:18 +0000

    Rename AboutPage to SearchPage and update the build method to reflect the new page content

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import '../app_layout.dart';
 
-class AboutPage extends StatelessWidget {
-  const AboutPage({super.key});
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
   @override
-  Widget build(BuildContext context) => const AppLayout(title: 'Union', child: Center(child: Text('About Page')));
+  Widget build(BuildContext context) => const AppLayout(title: 'Union', child: Center(child: Text('Search Page Content')));
 
   // This trailing comma makes auto-formatting nicer in code editors.
 }
