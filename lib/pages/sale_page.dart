9a3f1d43f6db57f655a7391eb716de436feafb8f
anana243 <anastasijan2005@gmail.com>
2025-12-02 20:40:28 +0000

    Add SalePage widget with basic layout structure

---
+++ lib/pages/sale_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../app_layout.dart';
+
+class SalePage extends StatelessWidget {
+  const SalePage({super.key});
+  @override
+  Widget build(BuildContext context) => const AppLayout(title: 'Union', child: Center(child: Text('Sale Page')));
+}
