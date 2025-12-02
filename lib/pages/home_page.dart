fcb4c6bd0230476f6b9a4542b0aa4f05016cc718
anana243 <anastasijan2005@gmail.com>
2025-12-02 20:39:11 +0000

    Add imports for ProductTile widget and Product model in SearchPage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../app_layout.dart';
+import '../widgets/product_tile.dart';
+import '../models/product.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
