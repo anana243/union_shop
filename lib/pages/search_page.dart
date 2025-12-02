e1a689707900b150654759476b5d37a419f469d3
anana243 <anastasijan2005@gmail.com>
2025-12-02 20:41:00 +0000

    Add SearchPage widget with basic layout structure

---
+++ lib/pages/search_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import '../app_layout.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+  @override
+  Widget build(BuildContext context) => const AppLayout(title: 'Union', child: Center(child: Text('Search Page Content')));
+}
