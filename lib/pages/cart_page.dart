d007649185e8286456560d3e58a421f0c8eb5a01
anana243 <anastasijan2005@gmail.com>
2025-12-03 19:43:24 +0000

    Remove unnecessary closing parenthesis in CartPage widget

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -69,7 +69,6 @@ class CartPage extends StatelessWidget {
                         );
                       },
                     ),
-                  ),
               ],
             );
           },
