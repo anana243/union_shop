1b39587799c38b21f4102906345d7819834c7610
anana243 <anastasijan2005@gmail.com>
2025-12-03 21:55:35 +0000

    Format PrintShackPage widget code for improved readability

--- lib/pages/print_shack_page.dart
+++ lib/pages/print_shack_page.dart
@@ -4,5 +4,6 @@ import '../app_layout.dart';
 class PrintShackPage extends StatelessWidget {
   const PrintShackPage({super.key});
   @override
-  Widget build(BuildContext context) => const AppLayout(title: 'Union', child: Center(child: Text('Print Shack Page')));
+  Widget build(BuildContext context) => const AppLayout(
+      title: 'Union', child: Center(child: Text('Print Shack Page')));
 }
