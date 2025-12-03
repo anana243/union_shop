34c2d1313df0fdcc7708210c3ad98ba05e879919
anana243 <anastasijan2005@gmail.com>
2025-12-03 21:55:01 +0000

    Format ProductGrid constructor for improved readability

--- lib/widgets/product_grid.dart
+++ lib/widgets/product_grid.dart
@@ -5,7 +5,8 @@ import 'product_tile.dart';
 class ProductGrid extends StatelessWidget {
   final List<Product> products;
   final bool twoColumnGrid;
-  const ProductGrid({super.key, required this.products, this.twoColumnGrid = false});
+  const ProductGrid(
+      {super.key, required this.products, this.twoColumnGrid = false});
 
   @override
   Widget build(BuildContext context) {
