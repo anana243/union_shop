59b47f95cf600ee17b7d3f1471c9662143073f3d
anana243 <anastasijan2005@gmail.com>
2025-12-03 21:55:48 +0000

    Format itemBuilder in PridePage widget code for improved readability

--- lib/pages/pride_page.dart
+++ lib/pages/pride_page.dart
@@ -55,7 +55,8 @@ class PridePage extends StatelessWidget {
                     childAspectRatio: 0.8,
                   ),
                   itemCount: products.length,
-                  itemBuilder: (context, i) => ProductTile(product: products[i]),
+                  itemBuilder: (context, i) =>
+                      ProductTile(product: products[i]),
                 );
               },
             ),
