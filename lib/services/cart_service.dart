1197e6cc499654f1f80bc2bafdd5b86c15ae5ba0
anana243 <anastasijan2005@gmail.com>
2025-12-03 20:16:29 +0000

    Refactor CartService to use CartItem for better quantity management and update item handling

--- lib/services/cart_service.dart
+++ lib/services/cart_service.dart
@@ -1,23 +1,44 @@
 import 'package:flutter/foundation.dart';
 import '../models/product.dart';
 
+class CartItem {
+  final Product product;
+  int quantity;
+
+  CartItem({required this.product, this.quantity = 1});
+}
+
 // Simple observable cart singleton
 class CartService extends ChangeNotifier {
   CartService._();
   static final CartService instance = CartService._();
 
-  final List<Product> _items = [];
+  final Map<String, CartItem> _items = {};
 
-  List<Product> get items => List.unmodifiable(_items);
-  int get count => _items.length;
+  List<CartItem> get items => _items.values.toList();
+  int get count => _items.values.fold(0, (sum, item) => sum + item.quantity);
+  double get total => _items.values.fold(0.0, (sum, item) => sum + (item.product.price * item.quantity));
 
   void add(Product p) {
-    _items.add(p);
+    if (_items.containsKey(p.id)) {
+      _items[p.id]!.quantity++;
+    } else {
+      _items[p.id] = CartItem(product: p);
+    }
     notifyListeners();
   }
 
   void remove(Product p) {
-    _items.removeWhere((i) => i.id == p.id);
+    _items.remove(p.id);
+    notifyListeners();
+  }
+
+  void updateQuantity(Product p, int quantity) {
+    if (quantity <= 0) {
+      _items.remove(p.id);
+    } else if (_items.containsKey(p.id)) {
+      _items[p.id]!.quantity = quantity;
+    }
     notifyListeners();
   }
 
