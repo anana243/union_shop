a4ebb0df3ce06fe166f74d68589124f4e0f5b8f5
anana243 <anastasijan2005@gmail.com>
2025-12-04 19:46:22 +0000

    Refactor ProductGrid constructor to use maxCrossAxisExtent for layout flexibility

--- lib/widgets/product_grid.dart
+++ lib/widgets/product_grid.dart
@@ -4,9 +4,8 @@ import 'product_tile.dart';
 
 class ProductGrid extends StatelessWidget {
   final List<Product> products;
-  final bool twoColumnGrid;
-  const ProductGrid(
-      {super.key, required this.products, this.twoColumnGrid = false});
+  final double? maxCrossAxisExtent;
+  const ProductGrid({super.key, required this.products, this.maxCrossAxisExtent});
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +13,8 @@ class ProductGrid extends StatelessWidget {
       child: LayoutBuilder(
         builder: (context, constraints) {
           final isDesktop = constraints.maxWidth >= 600;
-          final maxWidth = isDesktop ? 280.0 : 240.0;
-          
+          final maxWidth = maxCrossAxisExtent ?? (isDesktop ? 280.0 : 240.0);
+
           return Wrap(
             alignment: WrapAlignment.center,
             spacing: 24,
