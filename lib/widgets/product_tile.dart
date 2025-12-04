e68b8b2d5edfd17766ccba190aec66291a24f19e
anana243 <anastasijan2005@gmail.com>
2025-12-04 18:11:09 +0000

    Update padding in ProductTile for improved layout consistency

--- lib/widgets/product_tile.dart
+++ lib/widgets/product_tile.dart
@@ -67,7 +67,7 @@ class _ProductTileState extends State<ProductTile> {
                         ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.all(6.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisSize: MainAxisSize.min,
